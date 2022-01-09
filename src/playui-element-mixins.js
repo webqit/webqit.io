141b245c286dbbe71633bfaca1286fa66715e843
Oxford Harrison <oxharris.dev@gmail.com>
2022-01-09 16:44:45 +0100

    Debugging: header links active state

--- src/playui-element-mixins.js
+++ src/playui-element-mixins.js
@@ -232,16 +232,19 @@ export const _LinkItem = __LinkItem => class extends _Root(__LinkItem) {
     }
 
     isActivePage(href = this.state.href, uri = this.state.uri, documentUrl = document.state.url, documentUrlHref = document.state.url?.href, scrollSpyActive = this.state.scrollSpy?.active) {
-        console.log('::::::::::::', ...arguments);
         if ((!href && !uri) || !documentUrlHref) return;
         let match;
         if (uri || href.includes('#')) {
+            console.log('::::::::::::href.includes(\'#\')', href.includes('#'));
             match = this._matchQueryHash();
         } else if (href.includes('?')) {
+            console.log('::::::::::::href.includes(\'?\')', href.includes('?'));
             match = this._matchQueryParams();
         } else if (documentUrl.pathname) {
+            console.log('::::::::::::documentUrl.pathname', documentUrl.pathname);
             match = this._matchQueryPath();
         }
+        console.log('::::::::::::match', match);
         this.state.active = match.active;
         this.state.hasActive = match.hasActive;
         this.state.expanded = match.expanded;
